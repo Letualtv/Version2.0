2e8b32bd0f88e4e8bb1d37f373e972d9630ef953
Letual <letual@outlook.es>
2025-02-11 12:28:29 +0100

    actualizacion de db

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost'; // o la IP del servidor MySQL
-$dbname = 'phppuro'; // Nombre de tu base de datos
-$username = 'root'; // Tu usuario de base de datos
-$password = ''; // Tu contraseña de base de datos
+$host = 'PMYSQL187.dns-servicio.com:3306'; // o la IP del servidor MySQL
+$dbname = '10796594_encuestas_IESA'; // Nombre de tu base de datos
+$username = 'AP_admin'; // Tu usuario de base de datos
+$password = 'L37u4l*11'; // Tu contraseña de base de datos
 
 try {
     // Establecer la conexión utilizando PDO
